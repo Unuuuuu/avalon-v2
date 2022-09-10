f005303d97329067376441a1a54aec1ff3371a44
Yunwoo Ji <unu12073@gmail.com>
2022-09-10 17:59:06 +0900

    Update port

--- backend/index.ts
+++ backend/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT ?? 8080;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
