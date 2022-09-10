37cea27f5d57f4f4604ab61639ba814cccd8fac8
Yunwoo Ji <unu12073@gmail.com>
2022-09-11 00:43:12 +0900

    Add ts-node, nodemon

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
+import { createServer } from "http";
 import path from "path";
+import { Server } from "socket.io";
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 const port = process.env.PORT ?? 8080;
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+io.on("connection", (socket) => {
+  console.log("Connect", socket.id);
 });
 
-app.get("/api", (req, res) => {
+app.get("/status", (req, res) => {
   res.send("Hello World!");
 });
 
@@ -16,4 +20,8 @@ app.use(express.static(path.join(__dirname, "../../frontend/build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
+});
+
+httpServer.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
